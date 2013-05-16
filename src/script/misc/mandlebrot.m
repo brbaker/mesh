7a0ed5300e1e80a382c759729a85126d3bacb041
Brent Baker <brbaker@adobe.com>
2013-05-16 14:11:23 -0400

    Minor cleanup Still needed to declare the types for the tuples that are being passed around

--- src/script/misc/mandlebrot.m
+++ src/script/misc/mandlebrot.m
@@ -2,9 +2,9 @@
  * Code sample based off of http://rosettacode.org/wiki/Mandelbrot_set#Ruby
 */
 
-frangen(start:Double, extent:Double, step:Double)
+frangen(start, extent, step)
 {
-    stride:Double = fabs(step);
+    stride = fabs(step);
     n = f2i(fdivz(fabs(extent) +. stride -. 1.0, stride));
     count(fsign(extent) * n) | { start +. i2f($0) *. stride }
 };
@@ -12,27 +12,30 @@ ffromton(x, y, n)
 {
     frangen(x, if(flt(x, y), { y -. x +. 1.0 }, { fneg(x -. y) -. 1.0 }), n)
 };
-
-mandelbrot(a:(Double, Double)) -> (Double, Double)
-{
-    reduce({ a:((Double, Double), (Double, Double)) => 
-            result:(Double, Double) = a.0;
-            element:(Double, Double) = a.1;
-            rr:(Double, Double) = ((result.0 *. result.0) +. ((result.1 *. result.1) *. -1.0), (result.0 *. result.1) +. (result.1 *. result.0));
-            addz:(Double, Double) = ((rr.0 +. element.0), (rr.1 +. element.1));
-            (addz)
-            }, a, rep(50, a))
-};
-
-magnitude(a:(Double, Double)) -> Double
+magnitude(a:(Double, Double))
 {
     sqrt(fsq(a.0) +. fsq(a.1));
+};
+
+
+
+
+
+
+mandelbrot(a:(Double, Double))
+{
+    reduce({ a:((Double, Double), (Double, Double)) => 
+            result = a.0;
+            element = a.1;
+            // Calculate: result^2 + element
+            rr = (fsq(result.0) +. (fsq(result.1) *. -1.0), (result.0 *. result.1) +. (result.1 *. result.0));
+            ((rr.0 +. element.0), (rr.1 +. element.1));
+            }, a, rep(50, a))
 };
 
 ffromton(1.0, -1.0, 0.05) | {
     output = box([""]);
     ffromton(-2.0, 0.5, 0.0315) | {
-        //print(mandelbrot(($$0, $0)));
         if(flt(magnitude(mandelbrot(($0, $$0))), 2.0), { output <- { append($0, "*") } }, { output <- { append($0, " ") } });
     };
     print(strjoin(*output, ""));

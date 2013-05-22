544785046c8af997da9d8732d5b57de4c56d1d3f
Brent Baker <brbaker@adobe.com>
2013-05-22 09:18:58 -0400

    WIP mandlebrot

--- src/script/misc/mandlebrot.m
+++ src/script/misc/mandlebrot.m
@@ -30,13 +30,32 @@ mandelbrot(a:(Double, Double))
             // Calculate: result^2 + element
             rr = (fsq(result.0) +. (fsq(result.1) *. -1.0), (result.0 *. result.1) +. (result.1 *. result.0));
             ((rr.0 +. element.0), (rr.1 +. element.1));
-            }, a, rep(50, a))
+            }, a, rep(1000, a))
 };
 
+/*
 ffromton(1.0, -1.0, 0.05) | {
     output = box([""]);
     ffromton(-2.0, 0.5, 0.0315) | {
         if(flt(magnitude(mandelbrot(($0, $$0))), 2.0), { output <- { append($0, "*") } }, { output <- { append($0, " ") } });
     };
+    print(strjoin(*output, ""));
+}
+*/
+
+
+width = f2i(fdivz(fabs(-2.0) +. 0.5, 0.0165));
+ffromton(1.0, -1.0, 0.028) | {
+    output = box(rep(width+1, " "));
+    cols = ffromton(-2.0, 0.5, 0.0165); 
+    pforn( index(cols),
+        {
+        index => value = cols[index];
+        if(flt(magnitude(mandelbrot((value, $$0))), 2.0),
+            { output <- { listset($0, index, "*"); } },
+            {  output <- { listset($0, index, " "); } });
+        },
+        1
+    );
     print(strjoin(*output, ""));
 }
